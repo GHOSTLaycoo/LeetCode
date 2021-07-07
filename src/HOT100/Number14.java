5269cf0d9ce4a3db21e74ad559db8fea18fe0e1d
吴奇胜 <2627233351@qq.com>
2021-07-07 23:58:44 +0800

    最长有效括号

---
+++ src/HOT100/Number14.java
@@ -0,0 +1,55 @@
+package HOT100;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+public class Number14 {
+
+    /**
+     *
+     * 最长有效括号
+     *
+     *给你一个只包含 '('和 ')'的字符串，找出最长有效（格式正确且连续）括号子串的长度。
+     *
+     *
+     *
+     * 示例 1：
+     *
+     * 输入：s = "(()"
+     * 输出：2
+     * 解释：最长有效括号子串是 "()"
+     * 示例 2：
+     *
+     * 输入：s = ")()())"
+     * 输出：4
+     * 解释：最长有效括号子串是 "()()"
+     * 示例 3：
+     *
+     * 输入：s = ""
+     * 输出：0
+     *
+     * 来源：力扣（LeetCode）
+     * 链接：https://leetcode-cn.com/problems/longest-valid-parentheses
+     * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+     */
+
+    public int longestValidParentheses(String s) {
+        int maxans = 0;
+        Deque<Integer> stack = new LinkedList<Integer>();
+        stack.push(-1);
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(') {
+                stack.push(i);
+            } else {
+                stack.pop();
+                if (stack.isEmpty()) {
+                    stack.push(i);
+                } else {
+                    maxans = Math.max(maxans, i - stack.peek());
+                }
+            }
+        }
+        return maxans;
+    }
+
+}
